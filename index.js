0151af44d2f8688a8af18b89f8ccdedf6f56b92e
CallMe AsYouFeel <futaiwata@gmail.com>
2021-05-07 04:15:59 +0900

    add expanding feature

--- index.js
+++ index.js
@@ -1,6 +1,53 @@
 const Discord = require('discord.js');
 require('dotenv').config();
 
-dclient.on('ready', () => console.log(`Logged in as ${dclient.user.tag}!`));
+const client = new Discord.Client();
+
+const regurl = /https:\/\/discord(app)?.com\/channels(\/\d{18}){3}/g;
+
+client.on('ready', () => console.log(`Logged in as ${client.user.tag}!`));
+
+client.on('message', msg => {
+    let msgurls = msg.content.matchAll(regurl);
+    for (let msgurl of msgurls) {
+        let ids = msgurl[0].split('/').slice(4);
+        if (ids[0] != msg.guild.id) {
+            console.log('invalid guild: ' + msg.guild.id);
+            continue;
+        }
+        let cnl = msg.guild.channels.cache.get(ids[1]);
+        if (!cnl.viewable) {
+            console.log('not viewable: ' + cnl.id);
+            continue;
+        }
+        cnl.messages.fetch(ids[2]).then(target => {
+            let name;
+            if (target.member) {
+                name = target.member.displayName;
+            }else{
+                name = target.author.username;
+            }
+            let msgembed = new Discord.MessageEmbed({
+                author: {
+                    name: name,
+                    icon_url: target.author.avatarURL()
+                },
+                description: target.content,
+                timestamp: target.createdAt,
+                footer: {
+                    text: cnl.parent.name + ' > ' + cnl.name,
+                    icon_url: msg.guild.iconURL()
+                }
+            });
+            let attaches = target.attachment;
+            if (attaches) msgembed.setImage(attaches.array()[0].proxyURL);
+            let embeds = target.embeds;
+            if (embeds.length) msgembed.description += '\n(Original message was embedded.)';
+            msg.channel.send(msgembed);
+        });
+    };
+});
+
+client.on('channelCreate', channel => channel.fetch());
 
 client.login(process.env.BOT_TOKEN);
