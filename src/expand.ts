393ef93c0d5811767176635f9c0b9c31df49a30c
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2021-12-21 01:44:34 +0900

    strict: true and refactor

--- src/expand.ts
+++ src/expand.ts
@@ -1,54 +1,77 @@
-import { Client, GuildTextBasedChannel, Message, MessageEmbed, NewsChannel, Permissions, TextChannel, ThreadChannel } from "discord.js";
+import {
+    Client,
+    GuildTextBasedChannel,
+    Message,
+    MessageEmbed,
+    Permissions,
+} from "discord.js";
 
-export default function expand(client: Client, message: Message) {
-    async function getMessage(url: string): Promise<[GuildTextBasedChannel, Message]> {
-        let ids = url.split('/').slice(4);
-        if (ids[0] != message.guild.id) throw new Error(`\`${url}\`\nis not from this server. I could not expand it.`);
+export default function expand(client: Client<true>, message: Message) {
+    async function getMessage(
+        url: string
+    ): Promise<[GuildTextBasedChannel, Message]> {
+        let ids = url.split("/").slice(4);
+        if (ids[0] != message.guild?.id)
+            throw new Error(
+                `\`${url}\`\nis not from this server. I could not expand it.`
+            );
 
         let channel = await message.guild.channels.fetch(ids[1]);
-        if (!(channel.isText() || channel.isThread())) throw new Error(`\`${channel}\` is not a text channel.`);
+        if (!channel) throw new Error(`channel with id ${ids[1]} not found.`);
 
-        let allowed = channel.permissionsFor(client.user).has(Permissions.FLAGS.READ_MESSAGE_HISTORY);
-        if (!allowed) throw new Error(`I didn't have permission to see \n\`${url}\`.\nI could not expand it.`);
+        if (!(channel.isText() || channel.isThread()))
+            throw new Error(`\`${channel}\` is not a text channel.`);
+
+        let allowed = channel
+            .permissionsFor(client.user)
+            ?.has(Permissions.FLAGS.READ_MESSAGE_HISTORY);
+        if (!allowed)
+            throw new Error(
+                `I didn't have permission to see \n\`${url}\`.\nI could not expand it.`
+            );
 
         return [channel, await channel.messages.fetch(ids[2])];
     }
 
-    function createEmbeds(channel: GuildTextBasedChannel, target: Message) {
-        let name = target.member ? target.member.displayName : target.author.username;
-    
-        let channelDesc = '';
-        if (channel.parent.parent) channelDesc += channel.parent.parent.name + ' > ';
-        if (channel.parent) channelDesc += channel.parent.name + ' > ';
+    function createEmbeds([
+        channel,
+        { member, author, content, createdAt, attachments, embeds },
+    ]: [GuildTextBasedChannel, Message]) {
+        let name = member ? member.displayName : author.username;
+
+        let channelDesc = "";
+        if (channel.parent?.parent)
+            channelDesc += channel.parent?.parent.name + " > ";
+        if (channel.parent) channelDesc += channel.parent.name + " > ";
         channelDesc += channel.name;
-    
+
         let embed = new MessageEmbed({
             author: {
                 name: name,
-                icon_url: target.member.displayAvatarURL()
+                icon_url: (member ?? author).displayAvatarURL(),
             },
-            description: target.content,
-            timestamp: target.createdAt,
+            description: content,
+            timestamp: createdAt,
             footer: {
                 text: channelDesc,
-                icon_url: message.guild.iconURL()
-            }
+                icon_url: message.guild?.iconURL() ?? undefined,
+            },
         });
-    
-        let attach = target.attachments.find(att => att.width > 0);
+
+        let attach = attachments.find((att) => !!att.width);
         if (attach) embed.setImage(attach.url);
-    
-        let embeds = target.embeds;
-        if (embeds.length) embed.description += `\n(${embeds.length} ${embeds.length == 1 ? 'embed follows.' : 'embeds follow.'})`;
-    
+        if (embeds.length)
+            embed.description += `\n(${embeds.length} ${
+                embeds.length == 1 ? "embed follows." : "embeds follow."
+            })`;
+
         return [embed, ...embeds];
     }
 
-    return async (url: string) => {
-        let [channel, target] = await getMessage(url);
-        let embeds = createEmbeds(channel, target);
-
-        message.channel.send({ embeds })
-            .catch(e => console.error(e));
-    };
+    return (url: string) =>
+        getMessage(url)
+            .then(createEmbeds)
+            .then((embeds) => message.channel.send({ embeds }))
+            .catch((error) => message.channel.send(`${error as Error}`))
+            .catch(console.error);
 }
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,28 @@
-import { Client, Intents } from 'discord.js';
-import env from './env';
-import expand from './expand';
+import { Client, Intents } from "discord.js";
+import env from "./env";
+import expand from "./expand";
 
 const { BOT_TOKEN } = env;
 
 const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILDS
-    ]
+    intents: [Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILDS],
 });
 
 const regurl = /https:\/\/discord(app)?.com\/channels(\/\d{18}){3}/g;
 
-client.on('ready', client => {
+client.on("ready", (client) => {
     console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('messageCreate', message => {
+client.on("messageCreate", (message) => {
     if (message.author.bot) return;
-    let msgurls = message.content.match(regurl);
-    if (!msgurls) return;
+    let urls = message.content.match(regurl);
+    if (!(urls && urls.length < 6)) return;
 
-    msgurls.forEach(expand(client, message));
+    urls.forEach(expand(client, message));
 });
 
-client.on('threadCreate', channel => {
+client.on("threadCreate", (channel) => {
     channel.join().catch(console.error);
 });
 
