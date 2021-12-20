387cb922c14b553f199a947ee1712d9982c2ea1e
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2021-12-21 00:58:52 +0900

    refactor

---
+++ src/env.ts
@@ -0,0 +1,7 @@
+import readenv from '@cm-ayf/readenv';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const env = readenv({ BOT_TOKEN: {} });
+export default env;
--- src/expand.ts
+++ src/expand.ts
@@ -1,50 +1,38 @@
-const { Client, Intents, Permissions, MessageEmbed } = require('discord.js');
-require('dotenv').config();
+import { Client, Message, MessageEmbed, Permissions } from "discord.js";
 
-const client = new Client({
-    intents: [
-        Intents.FLAGS.GUILD_MESSAGES,
-        Intents.FLAGS.GUILDS
-    ]
-});
 
-const regurl = /https:\/\/discord(app)?.com\/channels(\/\d{18}){3}/g;
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}!`);
-});
-
-client.on('messageCreate', msg => {
-    if (msg.author.bot) return;
-    let msgurls = msg.content.match(regurl);
-    if (!msgurls) return;
-
-    msgurls.forEach(async msgurl => {
+export default function expand(client: Client, msg: Message) {
+    return async msgurl => {
         let ids = msgurl.split('/').slice(4);
         if (ids[0] != msg.guild.id) {
             msg.channel.send(`\`${msgurl}\`\nis not from this server. I could not expand it.`)
                 .catch(e => console.error(e));
             return;
         }
-
+    
         let cnl = await msg.guild.channels.fetch(ids[1]);
         let allowed = cnl.permissionsFor(client.user).has(Permissions.FLAGS.READ_MESSAGE_HISTORY);
+        if (!cnl.isText()) {
+            msg.channel.send(`I didn't have permission to see \n\`${msgurl}\`.\nI could not expand it.`)
+                .catch(e => console.error(e));
+            return;
+        }
         if (!allowed) {
             msg.channel.send(`I didn't have permission to see \n\`${msgurl}\`.\nI could not expand it.`)
                 .catch(e => console.error(e));
             return;
         }
-
+    
         let target = await cnl.messages.fetch(ids[2]);
-
+    
         let name;
         if (target.member) {
             name = target.member.displayName;
         }else{
             name = target.author.username;
         }
-
-        let channel_name　= '';
+    
+        let channel_name = '';
         if (cnl.parent.parent) {
             channel_name += cnl.parent.parent.name + ' > ';
         }
@@ -52,7 +40,7 @@ client.on('messageCreate', msg => {
             channel_name += cnl.parent.name + ' > ';
         }
         channel_name += cnl.name;
-
+    
         let msgembed = new MessageEmbed({
             author: {
                 name: name,
@@ -65,22 +53,15 @@ client.on('messageCreate', msg => {
                 icon_url: msg.guild.iconURL()
             }
         });
-
-        let attach = target.attachments.find(att => att.width);
+    
+        let attach = target.attachments.find(att => att.width > 0);
         if (attach) msgembed.setImage(attach.url);
-
+    
         let embeds = target.embeds;
         if (embeds.length) msgembed.description += `\n(${embeds.length} ${embeds.length == 1 ? 'embed follows.' : 'embeds follow.'})`;
-
+    
         msg.channel.send({
             embeds: [msgembed].concat(embeds)
         }).catch(e => console.error(e));
-    });
-});
-
-client.on('threadCreate', cnl => {
-    cnl.join()
-        .catch(e => console.error(e));
-});
-
-client.login(process.env.BOT_TOKEN).catch(e => console.error(e));
+    };
+}
