5d5762856ebfc06ae4a7c3fd6f6803d7d53feae1
CallMe AsYouFeel <cm.ayf2734@gmail.com>
2022-01-08 06:31:47 +0900

    dockerize

--- src/env.ts
+++ src/env.ts
@@ -1,7 +1,7 @@
 import readenv from '@cm-ayf/readenv';
-import dotenv from 'dotenv';
 
-dotenv.config();
+if (process.env.NODE_ENV !== 'production')
+  require('dotenv').config();
 
 const env = readenv({ BOT_TOKEN: {} });
 export default env;
